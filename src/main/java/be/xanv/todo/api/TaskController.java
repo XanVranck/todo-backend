bb05c2b37ec520517e0adb176d0e62a715839004
xanv <vranckaert.xan@gmail.com>
2021-01-15 16:51:49 +0100

    return http.OK

--- src/main/java/be/xanv/todo/api/TaskController.java
+++ src/main/java/be/xanv/todo/api/TaskController.java
@@ -2,11 +2,14 @@ package be.xanv.todo.api;
 
 import be.xanv.todo.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 @Transactional
@@ -17,6 +20,7 @@ public class TaskController {
     private TaskService taskService;
 
     @PostMapping(path = "/create")
+    @ResponseStatus(HttpStatus.OK)
     public void createTask(@RequestBody TaskDTO taskDTO) {
         taskService.createTask(taskDTO);
     }
