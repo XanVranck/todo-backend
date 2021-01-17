5bef4bbf864735a386f2f301662fcb2d649d3829
xanv <vranckaert.xan@gmail.com>
2021-01-17 15:52:42 +0100

    make package private

--- src/main/java/be/xanv/todo/service/TaskMapper.java
+++ src/main/java/be/xanv/todo/service/TaskMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class TaskMapper {
+class TaskMapper {
 
     List<TaskDTO> map(List<Task> tasks) {
         return tasks.stream()
